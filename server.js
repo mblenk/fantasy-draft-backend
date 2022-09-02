23d97d1a7aa709e8997327291375392d5ae662f8
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-09-02 12:27:41 +0100

    cors policy test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 //middleware
 app.use(express.json()) 
 app.use(cors({
-     origin: true, 
+     origin: 'https://solschfantasydraft.co.uk', 
      credentials: true 
 }))
 app.use(cookieParser())
