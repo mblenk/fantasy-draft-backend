cc1e2ba548477606c5a8dde808f19131a0ff6791
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-09-15 13:38:59 +0100

    do app test

--- server.js
+++ server.js
@@ -10,6 +10,51 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
+const leagueCode = 8548
+
+const playerIds = [
+    {
+        name:"Matt",
+        entry_id:75081,
+        id:75140
+    },
+    {
+        name:"Tom",
+        entry_id:74697,
+        id:74756
+    },
+    {
+        name:"Dan",
+        entry_id:138413,
+        id:138578
+    },
+    {
+        name:"Nick",
+        entry_id:72961,
+        id:73019
+    },
+    {
+        name:"James",
+        entry_id:184489,
+        id:184752
+    },
+    {
+        name:"Sam",
+        entry_id:143292,
+        id:143463
+    },
+    {
+        name:"Ollie",
+        entry_id:62522,
+        id:62565
+    },
+    {
+        name:"Doug",
+        entry_id:28784,
+        id:28800
+    }
+]
+
 //middleware
 app.use(express.json()) 
 app.use(cors({
