12543eac984ac4217874060a2a42c1d09593e5f9
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-09-21 12:15:35 +0100

    update route removed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/login', userController.login_user)
 router.post('/signup', userController.create_user)
-router.patch('/update', userController.update_user)
+// router.patch('/update', userController.update_user)
 router.get('/logout', userController.log_out)
 router.get('/check', userCheck, userController.check_user)
 
