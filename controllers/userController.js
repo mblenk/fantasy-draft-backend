30bab4de372dcb8aeabf55b62e0eb10dae312905
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-10-03 12:10:31 +0100

    guest access endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,22 @@
 const { ObjectId } = require('mongodb')
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer')
 require('dotenv').config()
+const {
+    handleErrors,
+    createToken,
+    createGuestToken,
+    createEmailTextLink
+} = require('./userFunctions')
 
-const handleErrors = (err) => {
-    console.log(err.message, err.code);
-    let errors = { username: '', password: '' };
-
-    // incorrect username
-    if(err.message === 'Incorrect username') {
-        errors.username = 'That username is not registered';
+const transporter = nodemailer.createTransport({
+    service:"outlook",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
-
-    // incorrect password
-    if(err.message === 'Incorrect password') {
-        errors.password = 'Password is incorrect';
-    }
-  
-    // duplicate username error
-    if(err.code === 11000) {
-      errors.username = 'That username is already registered';
-      return errors;
-    }
-  
-    // validation errors
-    if(err.message.includes('user validation failed')) {
-      Object.values(err.errors).forEach(({ properties }) => {
-        errors[properties.path] = properties.message;
-      });
-    }
-
-    return errors;
-  }
-
-const maxAge = 1 * 12 * 60 * 60 // days hours minutes seconds
-const createToken = (id) => {
-    return jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: maxAge
-    })
-}
+})
 
 
 module.exports.login_user = async (req, res) => {
@@ -103,8 +81,90 @@ module.exports.log_out = (req, res) => {
     res.status(200).json({ mssg: 'User logged out'})
 }
 
-module.exports._create_guest_user = async (req, res) => {
 
+
+module.exports.create_guest_user = async (req, res) => {
+    const { email } = req.body
+    const host = req.get('host')
+
+    // if(host !== 'localhost:3000') {
+    //     console.log('Access blocked, invalid host')
+    //     res.status(400).send('Invalid host')
+    //     return
+    // }
+
+    if(!email){
+        res.status(400).send('No email provided')
+        return
+    }
+
+    const guestUserIdNumber = Math.floor(Math.random() * (50 - 1 + 1) + 1)
+    const passwordKeyOne = Math.floor(Math.random() * (50 - 1 + 1) + 1)
+    const passwordKeyTwo = Math.floor(Math.random() * (50 - 1 + 1) + 1)
+
+    const timeNow = Date.now()
+    const expiryTime = new Date(timeNow + 30 * 60000)
+
+    try {
+        const user = await User.create({ 
+            display_name: `portfolioGuest${guestUserIdNumber}`, 
+            username: `portfolioGuest${guestUserIdNumber}`, 
+            password: `gup${guestUserIdNumber}${passwordKeyOne}${passwordKeyTwo}`, 
+            visits: 0, 
+            lastVisit: "01/01/2022", 
+            expiresAt: expiryTime,
+            requests: {
+                live_gwk: 0,
+                transfers: 0,
+                months: 0,
+                previous_year: 0,
+                all_time: 0,
+                random_trades: 0,
+                random_draft: 0
+            }
+        })
+        const token = createGuestToken(user._id)
+
+        const emailText = createEmailTextLink(token)
+
+        const nodemailerOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: "Your authorised access link",
+            text: emailText
+        }
+
+        transporter.sendMail(nodemailerOptions, (err, info) => {
+            if(err){
+                console.log(err)
+                return
+            }
+            console.log(info.response)
+        })
+
+        res.status(200).send('Access created')
+
+    }
+    catch(err) {
+        res.status(400).send(err.message);
+    }
+    
+    
+}
+
+module.exports.check_guest_user = async (req, res) => {
+    const { token } = req.body
+
+    jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+        if(err) {
+            console.log('JWT Error', err.message)
+            res.status(403).send(err.message)
+            throw new Error('Issue with token')
+        }
+        if(decodedToken) {
+            res.status(200).json({ token })
+        }  
+    })  
 }
 
 // module.exports.update_user = async (req, res) => {
